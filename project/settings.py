7dbec0acc26dfcc4af248a37b3b017230106c0fe
abdelrahman ibrahem <abdo.ibrahem1122@gmail.com>
2021-04-21 22:03:24 +0200

    Edit database from Sqqlite2 to postgresql

--- project/settings.py
+++ project/settings.py
@@ -77,13 +77,27 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_vezeta',
+        'USER': 'vezeta',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
